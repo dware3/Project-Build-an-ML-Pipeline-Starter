24744c7bee1e7d6e9c476c315e65324742f1c8d8
dware3 <dware3@wgu.edu>
2025-12-13 16:45:03 -0500

    Add data_check tests and wire data_check into pipeline

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ def go(config: DictConfig):
                 },
             )
 
+        # Basic data cleaning step
         if "basic_cleaning" in active_steps:
             _ = mlflow.run(
             os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
@@ -63,11 +64,22 @@ def go(config: DictConfig):
             },
         )
 
+        # Data check step
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "data_main"),
+            "main",
+            parameters={
+                # required artifacts
+                "csv": "clean_sample.csv:latest",
+                "ref": "clean_sample.csv:reference",
+
+                # parameters from config.yaml
+                "kl_threshold": config["data_check"]["kl_threshold"],
+                "min_price": config["etl"]["min_price"],
+                "max_price": config["etl"]["max_price"],
+            },
+        )
 
         if "data_split" in active_steps:
             ##################
